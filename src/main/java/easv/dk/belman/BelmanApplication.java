beec8ca63aa11395d2512a5d6b4a8dd35978eda3
Jianming Fan <151937346+Jianming2024@users.noreply.github.com>
2025-04-14 01:49:21 +0200

    create login View
    first commit

--- src/main/java/easv/dk/belman/BelmanApplication.java
+++ src/main/java/easv/dk/belman/BelmanApplication.java
@@ -2,17 +2,20 @@ package easv.dk.belman;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class BelmanApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(getClass().getResource("views/LoginView.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+        stage.setTitle("Log in View");
         stage.setScene(scene);
         stage.show();
     }
